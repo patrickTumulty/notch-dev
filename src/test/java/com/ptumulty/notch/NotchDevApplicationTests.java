713068231c5f12420acca3afcfa0bbe290124e3b
ptumulty <ptumulty1@gmail.com>
2021-08-10 23:48:52 -0700

    Initial Commit

---
+++ src/test/java/com/ptumulty/notch/NotchDevApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ptumulty.notch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotchDevApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
